f1d16edca0503614ccfffb3bd8f1ae36ce0e6152
grysivv <grysivv@gmail.com>
2026-05-29 16:25:49 +0200

    Initial Commit

---
+++ Con CalmaTests/Con_CalmaTests.swift
@@ -0,0 +1,18 @@
+//
+//  Con_CalmaTests.swift
+//  Con CalmaTests
+//
+//  Created by Daniel Gryś on 29/05/2026.
+//
+
+import Testing
+
+struct Con_CalmaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
